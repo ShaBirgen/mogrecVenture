dcac0d81d4f1e75ef353c05a259d8913610ae36e
ShaBirgen <sharoncherotich112@gmail.com>
2024-02-15 10:34:52 +0300

    Register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, RegisterComponent, LoginComponent
+  , NavbarComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
+})
+export class ProfileComponent {
+  link = ('/Frontend/src/app/assets/Mogrec Ventures png-02.png');
+}
