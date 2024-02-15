100d62c3f5e9f2101e71f826dc7b39554899d3f1
ShaBirgen <sharoncherotich112@gmail.com>
2024-02-15 16:28:03 +0300

    Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, LoginComponent, RegisterComponent, LoginComponent
-  , NavbarComponent, ProfileComponent],
+  , NavbarComponent, ProfileComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, HomeComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
