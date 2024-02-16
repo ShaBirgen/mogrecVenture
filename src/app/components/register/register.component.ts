79c4a149b45b0f5864b81c06c5a5644e3a83af0a
ShaBirgen <sharoncherotich112@gmail.com>
2024-02-16 09:30:53 +0300

    Routes

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HomeComponent } from '../home/home.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [NavbarComponent, HomeComponent],
+  imports: [NavbarComponent, HomeComponent, LoginComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
