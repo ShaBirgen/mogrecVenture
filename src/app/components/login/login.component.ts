50985bec7dc4b3f9198537ea253c61be33e5378b
ShaBirgen <sharoncherotich112@gmail.com>
2024-02-17 12:11:07 +0300

    Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterLink } from '@angular/router';
+import { FormsModule,FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  onSubmit(details: { email: string; password: string }) {
+    console.log(details);
+  }
 }
