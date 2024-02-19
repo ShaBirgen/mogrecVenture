eddea4ee094b008d919949458bb8edbcf6f5050e
ShaBirgen <sharoncherotich112@gmail.com>
2024-02-19 15:29:05 +0300

    Sliders

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,8 +7,34 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [NavbarComponent, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  currentIndex = 0;
 
+  showSlide(index: number) {
+    const slides = document.querySelectorAll('.slide');
+    console.log('This is slides');
+
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.setAttribute('data-active', '');
+      } else {
+        slide.removeAttribute('data-active');
+      }
+    });
+  }
+
+  prevSlide() {
+    const slides = document.querySelectorAll('.slide');
+    this.currentIndex = (this.currentIndex - 1 + slides.length) % slides.length;
+    this.showSlide(this.currentIndex);
+    console.log('Clicked');
+  }
+
+  nextSlide() {
+    const slides = document.querySelectorAll('.slide');
+    this.currentIndex = (this.currentIndex + 1) % slides.length;
+    this.showSlide(this.currentIndex);
+  }
 }
